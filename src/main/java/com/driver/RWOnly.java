3c691c1e152f2b1746702cdf9209aa0794b50616
chandrahasan <chandrahasan667@gmail.com>
2022-12-11 22:38:03 +0530

    message

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public void setName(String name1){
+        name = name1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
